0918bee76ba68ed4b5d86de0f0634f027970f47d
akonkwa <justinakonwa0@gmail.com>
2024-10-21 17:12:02 +0100

    add AddPlanEpargne page

---
+++ lib/Adds/add_plan_Epargne.dart
@@ -0,0 +1,280 @@
+import 'package:balare/screens/plan_epargne_screen.dart';
+import 'package:balare/widget/app_text.dart';
+import 'package:balare/widget/app_text_large.dart';
+import 'package:balare/widget/bouton_next.dart';
+import 'package:balare/widget/constantes.dart';
+import 'package:balare/widget/textfield.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class AddPlanEpargne extends StatefulWidget {
+  const AddPlanEpargne({super.key});
+
+  @override
+  State<AddPlanEpargne> createState() => _AddPlanEpargneState();
+}
+
+class _AddPlanEpargneState extends State<AddPlanEpargne> {
+  final TextEditingController _nomController = TextEditingController();
+  final TextEditingController _objectifController = TextEditingController();
+  DateTime? _selectedDate;
+  String _selectedFrequency = 'défaut';
+
+  Future<void> _selectDate(BuildContext context) async {
+    final DateTime? picked = await showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(2000),
+      lastDate: DateTime(2101),
+    );
+    if (picked != null && picked != _selectedDate) {
+      setState(() {
+        _selectedDate = picked;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: MediaQuery.of(context).viewInsets,
+      child: Container(
+        padding: EdgeInsets.only(left: 15, right: 15, top: 20, bottom: 30),
+        height: 450,
+        decoration: BoxDecoration(
+          color: Theme.of(context).colorScheme.background,
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(20),
+            topRight: Radius.circular(20),
+          ),
+        ),
+        child: StatefulBuilder(
+          builder: (BuildContext context, StateSetter setModalState) {
+            return Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    AppTextLarge(text: 'Ajouter une épargne'),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.of(context).pop();
+                      },
+                      child: Icon(
+                        Icons.cancel_outlined,
+                        color: Colors.red,
+                      ),
+                    )
+                  ],
+                ),
+                sizedbox, sizedbox,
+                sizedbox,
+                textfield(
+                  context,
+                  "Nom de l\'épargne",
+                  _nomController,
+                  double.maxFinite,
+                  Icon(Icons.text_fields_sharp),
+                ),
+                sizedbox,
+                sizedbox,
+                textfield(
+                  context,
+                  "Objectif \$",
+                  _objectifController,
+                  double.maxFinite,
+                  Icon(Icons.bar_chart),
+                ),
+
+                sizedbox,
+                sizedbox,
+                // Sélection de la date
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Container(
+                      alignment: Alignment.center,
+                      width: MediaQuery.of(context).size.width * 0.78,
+                      height: 50,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15.0),
+                        border: Border.all(
+                          color: Theme.of(context).highlightColor,
+                        ),
+                      ),
+                      child: AppText(
+                        text: _selectedDate == null
+                            ? 'Choisir une date'
+                            : 'Date : ${_selectedDate!.day}/${_selectedDate!.month}/${_selectedDate!.year}',
+                      ),
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        _selectDate(context).then((_) {
+                          setModalState(() {
+                            // Rafraîchir l'état du modal après avoir sélectionné la date
+                          });
+                        });
+                      },
+                      child: Container(
+                        height: 50,
+                        width: 50,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(15.0),
+                          border: Border.all(
+                            color: Theme.of(context).highlightColor,
+                          ),
+                        ),
+                        child: Icon(
+                          Icons.arrow_drop_down,
+                          size: 40,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+                sizedbox,
+                sizedbox,
+
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Container(
+                      alignment: Alignment.center,
+                      width: MediaQuery.of(context).size.width * 0.78,
+                      height: 50,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15.0),
+                        border: Border.all(
+                          color: Theme.of(context).highlightColor,
+                        ),
+                      ),
+                      child: AppText(
+                        text: _selectedFrequency,
+                      ),
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        _selectDate(context).then((_) {
+                          setModalState(() {
+                            // Rafraîchir l'état du modal après avoir sélectionné la date
+                          });
+                        });
+                      },
+                      child: Container(
+                        alignment: Alignment.center,
+                        height: 50,
+                        width:
+                            50.0, // S'étendre sur toute la largeur disponible
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(15.0),
+                          border: Border.all(
+                            color: Theme.of(context).highlightColor,
+                          ),
+                        ),
+                        child: DropdownButtonHideUnderline(
+                          // Cacher la ligne sous le DropdownButton
+                          child: DropdownButton<String>(
+                            padding: EdgeInsets.only(right: 7.0),
+                            icon: Center(
+                              child: Icon(
+                                Icons.arrow_drop_down,
+                                size: 35.0,
+                              ),
+                            ),
+                            isExpanded:
+                                true, // Permet au DropdownButton de prendre toute la largeur
+                            items: <String>[
+                              'Par Jour',
+                              'Par Semaine',
+                              'par Mois',
+                              'défaut',
+                            ].map<DropdownMenuItem<String>>((String value) {
+                              return DropdownMenuItem<String>(
+                                value: value,
+                                child: AppText(text: value),
+                              );
+                            }).toList(),
+                            onChanged: (String? newValue) {
+                              setModalState(() {
+                                _selectedFrequency = newValue!;
+                              });
+                            },
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+                Spacer(),
+
+                NextButton(
+                  onTap: () async {
+                    await _saveEpargneToFirebase();
+                    Navigator.pop(context);
+                  },
+                  child: AppText(
+                    text: 'Valider',
+                    color: Theme.of(context).colorScheme.surface,
+                  ),
+                ),
+              ],
+            );
+          },
+        ),
+      ),
+    );
+  }
+
+  Future<void> _saveEpargneToFirebase() async {
+    String userId = (await FirebaseAuth.instance.currentUser)?.uid ?? '';
+
+    String nom = _nomController.text;
+    String objectif = _objectifController.text;
+    String dateLimite = _selectedDate != null
+        ? '${_selectedDate!.day}/${_selectedDate!.month}/${_selectedDate!.year}'
+        : 'Non défini';
+    String frequence = _selectedFrequency;
+
+    if (nom.isNotEmpty &&
+        objectif.isNotEmpty &&
+        dateLimite.isNotEmpty &&
+        frequence.isNotEmpty) {
+      try {
+        await FirebaseFirestore.instance
+            .collection('users')
+            .doc(userId)
+            .collection('epargnes')
+            .add({
+          'nom': nom,
+          'objectif': double.parse(objectif),
+          'date_limite': dateLimite,
+          'frequence': frequence,
+          'timestamp': FieldValue.serverTimestamp(),
+        });
+
+        // Clear les champs après soumission
+        _nomController.clear();
+        _objectifController.clear();
+        setState(() {
+          _selectedDate = null;
+          _selectedFrequency = 'défaut';
+        });
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => EpargneListPage()),
+        );
+        ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text('Épargne ajoutée avec succès')));
+      } catch (e) {
+        ScaffoldMessenger.of(context)
+            .showSnackBar(SnackBar(content: Text('Erreur : $e')));
+      }
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Veuillez remplir tous les champs')));
+    }
+  }
+}
--- lib/screens/plan_epargne_screen.dart
+++ lib/screens/plan_epargne_screen.dart
@@ -4,7 +4,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-
 class EpargneListPage extends StatelessWidget {
   const EpargneListPage({super.key});
 
@@ -39,42 +38,110 @@ class EpargneListPage extends StatelessWidget {
             itemCount: epargnes.length,
             itemBuilder: (context, index) {
               final epargne = epargnes[index];
-              final selectedEpargneId =
-                  epargne.id; // Récupérer l'ID du plan d'épargne
-          
-              return Card(
-                child: ListTile(
-                  title: AppText(text: epargne['nom']),
-                  subtitle: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      AppText(text: "Objectif : \$${epargne['objectif']}"),
-                      AppText(text: "Date limite : ${epargne['date_limite']}"),
-                      AppText(text: "Fréquence : ${epargne['frequence']}"),
-                    ],
-                  ),
-                  trailing: IconButton(
-                    icon: Icon(Icons.edit),
-                    onPressed: () {
-                      // Vous pouvez ajouter une fonction pour éditer le plan d'épargne
-                    },
-                  ),
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) =>
-                            ContributionListPage(epargneId: selectedEpargneId),
-                      ),
-                    );
-                    // Appeler le modal pour ajouter une contribution, en passant l'ID
+              final selectedEpargneId = epargne.id; // Récupérer l'ID du plan d'épargne
+
+              return ListTile(
+                title: AppText(text: epargne['nom']),
+                subtitle: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    AppText(text: "Objectif : \$${epargne['objectif']}"),
+                    AppText(text: "Date limite : ${epargne['date_limite']}"),
+                    AppText(text: "Fréquence : ${epargne['frequence']}"),
+                  ],
+                ),
+                trailing: IconButton(
+                  icon: Icon(Icons.edit),
+                  onPressed: () {
+                    _showEditEpargneModal(context, selectedEpargneId, epargne);
                   },
                 ),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) =>
+                          ContributionListPage(epargneId: selectedEpargneId),
+                    ),
+                  );
+                },
               );
             },
           );
         },
       ),
     );
+  }
+
+  void _showEditEpargneModal(BuildContext context, String epargneId, QueryDocumentSnapshot epargne) {
+    final TextEditingController nomController = TextEditingController(text: epargne['nom']);
+    final TextEditingController objectifController = TextEditingController(text: epargne['objectif'].toString());
+    final TextEditingController dateLimiteController = TextEditingController(text: epargne['date_limite']);
+    final TextEditingController frequenceController = TextEditingController(text: epargne['frequence']);
+
+    showModalBottomSheet(
+      context: context,
+      builder: (context) {
+        return Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              AppText(text: 'Modifier le Plan d\'Épargne', size: 20),
+              SizedBox(height: 10),
+              TextField(
+                controller: nomController,
+                decoration: InputDecoration(labelText: 'Nom'),
+              ),
+              TextField(
+                controller: objectifController,
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(labelText: 'Objectif \$'),
+              ),
+              TextField(
+                controller: dateLimiteController,
+                decoration: InputDecoration(labelText: 'Date Limite'),
+              ),
+              TextField(
+                controller: frequenceController,
+                decoration: InputDecoration(labelText: 'Fréquence'),
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () async {
+                  await _updateEpargne(epargneId, nomController.text, double.tryParse(objectifController.text) ?? 0, dateLimiteController.text, frequenceController.text,context);
+                  Navigator.pop(context);
+                },
+                child: Text('Sauvegarder'),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  Future<void> _updateEpargne(String epargneId, String nom, double objectif, String dateLimite, String frequence,BuildContext context) async {
+    String userId = (await FirebaseAuth.instance.currentUser)?.uid ?? '';
+
+    try {
+      await FirebaseFirestore.instance
+          .collection('users')
+          .doc(userId)
+          .collection('epargnes')
+          .doc(epargneId)
+          .update({
+        'nom': nom,
+        'objectif': objectif,
+        'date_limite': dateLimite,
+        'frequence': frequence,
+      });
+
+      ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Plan d\'épargne mis à jour avec succès')));
+    } catch (e) {
+      ScaffoldMessenger.of(context)
+          .showSnackBar(SnackBar(content: Text('Erreur : $e')));
+    }
   }
 }
